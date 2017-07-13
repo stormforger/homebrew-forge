0ff84c22489b34de2d5859cdc8e69da3e130ea69
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2017-07-13 23:30:41 +0200

    update to 0.4.0

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,11 +1,11 @@
 class Forge < Formula
-  VERSION = "0.3.1"
+  VERSION = "0.4.0"
 
   desc "The StormForger Command Line Client, called 'forge'"
   homepage "https://stormforger.com"
   url "https://github.com/stormforger/cli/releases/download/v#{VERSION}/forge_v#{VERSION}_darwin_amd64.zip"
   version VERSION
-  sha256 "14941ef48f8eb7111dae43044cec0d465be346c5b554f1c7f00b1589fc4f0567"
+  sha256 "ca735e0627f7b71304cf6c6c575f4e2b527195e018ce733f6ba90abc2df49888"
 
   def install
     bin.install Dir["*"]
