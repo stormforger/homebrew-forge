8b8d29dd2fdcff73dc787a6ff0c2138e66611ebf
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-14 17:13:20 +0200

    forge version v0.5.0

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -3,7 +3,7 @@ class Forge < Formula
   homepage "https://stormforger.com"
   url "https://github.com/stormforger/cli/releases/download/v0.5.0/forge_v0.5.0_darwin_amd64.zip"
   version "0.5.0"
-  sha256 "3a14bcfcc05cf5aa48eee9da97ff66c18a2daa065bf33c716cca413caac3281c"
+  sha256 "5b1e8bedd1607d49cca6a34f1da652c434977dffbb3ce9af189b9e623123666d"
 
   def install
     bin.install "forge"
