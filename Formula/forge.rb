f63b0731cd05c8ab6c005e0e9341d93c6d349217
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-27 23:22:36 +0100

    forge version v0.7.0

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,9 +1,9 @@
 class Forge < Formula
   desc "The StormForger Command Line Client, called 'forge'"
   homepage "https://stormforger.com"
-  url "https://github.com/stormforger/cli/releases/download/v0.6.1/forge_v0.6.1_darwin_amd64.zip"
-  version "0.6.1"
-  sha256 "78088a912731b401b5cb96a9dda5f29be3c7e46d492caf7bc3b38a6619d5cba6"
+  url "https://github.com/stormforger/cli/releases/download/v0.7.0/forge_v0.7.0_darwin_amd64.zip"
+  version "0.7.0"
+  sha256 "cc2f8110c98f5e775e9908632dbbe09ade290c468903f44cd916a31df69051f0"
 
   def install
     bin.install "forge"
