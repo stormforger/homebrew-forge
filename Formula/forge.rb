861bdf6fafeb12e0a8be4dc6f89c23c8913c1095
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-28 13:51:10 +0200

    Brew formula update for forge version v0.29.0

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,9 +1,9 @@
 class Forge < Formula
   desc "The StormForger Command Line Client, called 'forge'"
   homepage "https://stormforger.com"
-  url "https://github.com/stormforger/cli/releases/download/v0.28.0/forge_v0.28.0_darwin_amd64.zip"
-  version "0.28.0"
-  sha256 "8affdf81a0d34e27575e8948efb41342499410f8e93b27ff0c3711d85cc38db8"
+  url "https://github.com/stormforger/cli/releases/download/v0.29.0/forge_v0.29.0_darwin_amd64.zip"
+  version "0.29.0"
+  sha256 "67443dbf5a3194d8ce880fd505bfe8ee6914a92cbf8979f545e76e9b2350ad55"
 
   def install
     bin.install "forge"
