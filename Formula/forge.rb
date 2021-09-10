f13e8f38a3397fbcda73798859b88730becb050a
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-10 17:23:48 +0200

    Brew formula update for forge version v0.48.0

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -5,28 +5,28 @@
 class Forge < Formula
   desc "The StormForger Command Line Client, called 'forge'"
   homepage "https://stormforger.com"
-  version "0.47.0"
+  version "0.48.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/stormforger/cli/releases/download/v0.47.0/forge_v0.47.0_darwin_amd64.zip"
-      sha256 "ad0caff4d7e77e80ad1a93b554df25818c55ef8bc4cdcb04b5a7a5b1729d462e"
+      url "https://github.com/stormforger/cli/releases/download/v0.48.0/forge_v0.48.0_darwin_amd64.zip"
+      sha256 "15f486c0970d907d634dd229d15582b65f6a2e4d5615e880e829e4cc8f75874e"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/stormforger/cli/releases/download/v0.47.0/forge_v0.47.0_darwin_arm64.zip"
-      sha256 "2b8fa13fe35f8fc7df44e5fad287d8676df26b3f44b9bfadf53ebcd46cc6b0de"
+      url "https://github.com/stormforger/cli/releases/download/v0.48.0/forge_v0.48.0_darwin_arm64.zip"
+      sha256 "aa31c6534e92b013d0f35a98414914715666a40c64365f08b42d4c8810ee4e84"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/stormforger/cli/releases/download/v0.47.0/forge_v0.47.0_linux_amd64.tar.gz"
-      sha256 "390da762a68b3d07938365f5aadb5dcd58327c1ba8f735bb4519fc7af098ec64"
+      url "https://github.com/stormforger/cli/releases/download/v0.48.0/forge_v0.48.0_linux_amd64.tar.gz"
+      sha256 "d55fd6d2b9fdbd3497815aec2f2e9edc50ea1cf24d50fc4911d07626825ef3a6"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/stormforger/cli/releases/download/v0.47.0/forge_v0.47.0_linux_arm64.tar.gz"
-      sha256 "47561bd5b52d590fd5656c0f39c36f45e27f486a2742360311726dfc0b09ff81"
+      url "https://github.com/stormforger/cli/releases/download/v0.48.0/forge_v0.48.0_linux_arm64.tar.gz"
+      sha256 "eb82c325948dad07e5c4ca67dc8160280bdafaba20d8c351ea3f4b10ed3cf7e8"
     end
   end
 
